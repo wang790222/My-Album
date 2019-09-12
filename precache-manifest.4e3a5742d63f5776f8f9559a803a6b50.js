22c1df157a1c7313ea7654b3395d54f810db9ea7
wang790222 <wang790222@gmail.com>
2019-09-12 00:08:47 -0400

    Updates

---
+++ precache-manifest.4e3a5742d63f5776f8f9559a803a6b50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13daf4922aa0840915963aa077efd9f",
+    "url": "/My-Album/index.html"
+  },
+  {
+    "revision": "4410f3df185501eb6ae7",
+    "url": "/My-Album/static/css/main.8cb21e88.chunk.css"
+  },
+  {
+    "revision": "7414f7160a2acbcb4cc1",
+    "url": "/My-Album/static/js/2.5e897b3f.chunk.js"
+  },
+  {
+    "revision": "4410f3df185501eb6ae7",
+    "url": "/My-Album/static/js/main.0db81a4b.chunk.js"
+  },
+  {
+    "revision": "e93d70015cba003b4a00",
+    "url": "/My-Album/static/js/runtime~main.2b7ef477.js"
+  }
+]);
